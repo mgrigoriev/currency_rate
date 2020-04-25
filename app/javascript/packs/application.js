98da10ac78d35f32d19c5facbc390d379ba55c62
Mikhail Grigoriev <mgrigoriev@gmail.com>
2020-04-25 17:34:48 +0300

    Add Bootstrap 4

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,11 @@ require("@rails/activestorage").start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap'
+import '../stylesheets/application'
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
