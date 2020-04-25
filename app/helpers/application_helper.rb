e530d6336f1ff6ce3d1441153929a476e4215252
Mikhail Grigoriev <mgrigoriev@gmail.com>
2020-04-25 17:41:39 +0300

    Update layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@
 
 module ApplicationHelper
   def formatted_currency_rate(price)
-    number_to_currency(price, locale: 'ru', unit: '₽', precision: 2)
+    number_to_currency(price, locale: 'ru', unit: '₽', separator: ',', format: '%n %u')
   end
 end
