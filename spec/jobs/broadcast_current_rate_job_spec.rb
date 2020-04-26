db14e1c4e05e483fe714dba87f1ef6890ecf47f3
Mikhail Grigoriev <mgrigoriev@gmail.com>
2020-04-26 03:48:08 +0300

    Add specs for background jobs

---
+++ spec/jobs/broadcast_current_rate_job_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe BroadcastCurrentRateJob, type: :job do
+  before do
+    ActiveJob::Base.queue_adapter = :test
+  end
+
+  it 'enqueues job' do
+    expect { described_class.perform_later }.to have_enqueued_job(described_class)
+  end
+end
